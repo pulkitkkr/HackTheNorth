c333b75e682c1e62bf67b9f1632ce349fbd5a7dc
pulkitkkr <pulkitkkr@gmail.com>
2019-09-14 08:58:03 -0400

    Prettier Changes

--- App/Themes/Metrics.js
+++ App/Themes/Metrics.js
@@ -1,6 +1,6 @@
-import {Dimensions, Platform} from 'react-native'
+import { Dimensions, Platform } from "react-native";
 
-const { width, height } = Dimensions.get('window')
+const { width, height } = Dimensions.get("window");
 
 // Used via Metrics.baseMargin
 const metrics = {
@@ -14,7 +14,7 @@ const metrics = {
   horizontalLineHeight: 1,
   screenWidth: width < height ? width : height,
   screenHeight: width < height ? height : width,
-  navBarHeight: (Platform.OS === 'ios') ? 64 : 54,
+  navBarHeight: Platform.OS === "ios" ? 64 : 54,
   buttonRadius: 4,
   icons: {
     tiny: 15,
@@ -29,6 +29,6 @@ const metrics = {
     large: 60,
     logo: 200
   }
-}
+};
 
-export default metrics
+export default metrics;
