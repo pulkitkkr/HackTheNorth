c0f98f8bc608042c5bdbdbe9192666ee7cb5e4cf
tejasavkhattar <tejasav1997@gmail.com>
2019-09-15 03:33:07 -0400

    user details and logout

--- App/Containers/UserProfileScreen/UserProfileScreen.js
+++ App/Containers/UserProfileScreen/UserProfileScreen.js
@@ -3,10 +3,13 @@ import { ScrollView, View, Text } from "react-native";
 import { connect } from "react-redux";
 import { SectionWithHeading } from "../../Components/shared";
 import Icon from "react-native-vector-icons/AntDesign";
-
+import { CustomButton } from "../../Components/shared";
+import {
+  RESET_CUSTOMER_DETAILS
+} from "../../Redux/CustomerDetailRedux";
 const UserProfileScreen = props => {
-  const { givenName, age } = props.customerDetails.userDetails;
-
+  const { givenName, age, birthDate, type, gender, relationshipStatus, totalIncome, workActivity } = props.customerDetails.userDetails;
+  console.log(props.customerDetails.userDetails)
   return (
     <ScrollView>
       <View
@@ -25,25 +28,67 @@ const UserProfileScreen = props => {
       </View>
       <View style={{ paddingLeft: 15, paddingRight: 15 }}>
         <SectionWithHeading title={"About Me"}>
-          <View style={{ flexDirection: "row" }}>
+          <View style={{ flexDirection: "row", paddingBottom: 20 }}>
             <View style={{ flexDirection: "column", flex: 1 }}>
               <Text style={{ flex: 1, fontSize: 16, fontWeight: "600" }}>
-                detail:
+                Birth Date:
               </Text>
-              <Text style={{ fontSize: 16, flex: 1 }}>kljlkhlhjlkh</Text>
+              <Text style={{ fontSize: 16, flex: 1 }}>{birthDate}</Text>
             </View>
             <View style={{ flexDirection: "column", flex: 1 }}>
               <Text style={{ flex: 1, fontSize: 16, fontWeight: "600" }}>
-                detail:
+                Account Type:
               </Text>
-              <Text style={{ fontSize: 16, flex: 1 }}>kljlkhlhjlkh</Text>
+              <Text style={{ fontSize: 16, flex: 1 }}>{type}</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: "row" , paddingBottom: 20}}>
+            <View style={{ flexDirection: "column", flex: 1 }}>
+              <Text style={{ flex: 1, fontSize: 16, fontWeight: "600" }}>
+                Gender:
+              </Text>
+              <Text style={{ fontSize: 16, flex: 1 }}>{gender}</Text>
+            </View>
+            <View style={{ flexDirection: "column", flex: 1 }}>
+              <Text style={{ flex: 1, fontSize: 16, fontWeight: "600" }}>
+                Relationship Status:
+              </Text>
+              <Text style={{ fontSize: 16, flex: 1 }}>{relationshipStatus}</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: "row" , paddingBottom: 20}}>
+            <View style={{ flexDirection: "column", flex: 1 }}>
+              <Text style={{ flex: 1, fontSize: 16, fontWeight: "600" }}>
+                Total Income:
+              </Text>
+              <Text style={{ fontSize: 16, flex: 1 }}>{totalIncome}</Text>
+            </View>
+            <View style={{ flexDirection: "column", flex: 1 }}>
+              <Text style={{ flex: 1, fontSize: 16, fontWeight: "600" }}>
+                Work Type:
+              </Text>
+              <Text style={{ fontSize: 16, flex: 1 }}>{workActivity}</Text>
             </View>
           </View>
         </SectionWithHeading>
+        <CustomButton
+        onPress={() => {
+          props.resetState();
+          props.navigation.replace("LaunchScreen");
+          }}
+          title={"Log Out"}
+        />
       </View>
     </ScrollView>
   );
 };
+
+const mapDispatchToProps = dispatch => ({
+  resetState: ()=>
+    dispatch({
+      type: RESET_CUSTOMER_DETAILS,
+    })
+});
 
 const mapStateToProps = state => ({
   customerDetails: state.customerDetails
@@ -51,5 +96,6 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
+  mapDispatchToProps,
   null
 )(UserProfileScreen);
